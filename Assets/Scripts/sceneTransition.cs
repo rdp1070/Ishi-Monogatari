5167cda6a04cd6d74123e84853a119780e25c63f
Robert Pruden <rdp1070@rit.edu>
2017-02-11 14:31:59 -0500

    Place holder main menu

---
+++ Assets/Scripts/sceneTransition.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class sceneTransition : MonoBehaviour {
+
+    public string Destination;
+ 
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(Destination);
+    }
+
+}
